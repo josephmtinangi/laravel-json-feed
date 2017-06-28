305746c498b3a5cf2d29413144a70c832c78f18f
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-28 19:58:02 +0300

    Return feed

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use App\Post;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +20,34 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('feed/json', function () {
+    $posts = Post::limit(20)->get();
+
+    $data = [
+        'version' => '',
+        'title' => '',
+        'home_page_url' => '',
+        'feed_url' => '',
+        'icon' => '',
+        'favicon' => '',
+        'items' => [],
+    ];
+
+    foreach ($posts as $key => $post) {
+        $data['items'][$key] = [
+            'id' => $post->id,
+            'title' => $post->title,
+            'url' => $post->url,
+            'image' => $post->image,
+            'content_html' => $post->content_html,
+            'date_published' => $post->created_at->tz('UTC')->toRfc3339String(),
+            'date_modified' => $post->updated_at->tz('UTC')->toRfc3339String(),
+            'author' => [
+                'name' => $post->user->name
+            ],
+        ];
+    }
+
+    return $data;
+});
